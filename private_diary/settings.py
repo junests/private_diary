b11026668439ce80a6e9d26b599e22eae28952fd
junests <j.allest20201101@gmail.com>
2021-01-09 19:02:48 +0900

    TOPページ

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# static
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Logging
 Logging = {
